baffcb1502af9e20fe0108523cb3bc5063b27d3c
Mihir Prabhudesai <mihirp1998.mp@gmail.com>
2020-08-30 00:54:30 +0530

    multiprocess fixed

--- preprocess_shapenet.py
+++ preprocess_shapenet.py
@@ -1,138 +1,155 @@
 from __future__ import print_function
-import ipdb
+# import ipdb
 import math, sys, random, argparse, json, os, tempfile, pickle
 import mathutils
 import shutil
-import pdb
+# import pdb
 import subprocess
 import os
 import numpy as np
 import time
 import pathos.pools as pp
-st = ipdb.set_trace
+# st = ipdb.set_trace
 import bpy
 xmax, ymax, zmax = 5.0, 5.0, 5.0
-from preprocess_settings import *
+# from preprocess_settings import *
 # blender --background --python preprocess_shapenet.py
-def combine_objects():
-    """combine all objects in the scene
-    """
-    scene = bpy.context.scene
-    obs = []
-
-    for ob in scene.objects:
-    # whatever objects you want to join...
-        if ob.type == 'MESH':
-            obs.append(ob)
-
-    ctx = bpy.context.copy()
-    # one of the objects to join
-    ctx['active_object'] = obs[0]
-    ctx['selected_objects'] = obs
-    # we need the scene bases as well for joining
-    ctx['selected_editable_bases'] = [scene.object_bases[ob.name] for ob in obs]
-    bpy.ops.object.join(ctx)
-
-def move_obj_above_ground(mesh_obj):
-    minz = 999999.0
-    for vertex in mesh_obj.data.vertices:
-        # object vertices are in object space, translate to world space
-        v_world = mesh_obj.matrix_world * mathutils.Vector((vertex.co[0],vertex.co[1],vertex.co[2]))
-
-        if v_world[2] < minz:
-            minz = v_world[2]
-    
-    mesh_obj.location.z = mesh_obj.location.z - minz + 0.3
-
-def clear_mesh():
-    """ clear all meshes in the secene
-    """
-    bpy.ops.object.select_all(action='DESELECT')
-    for obj in bpy.data.objects:
-        if obj.type == 'MESH':
-            obj.select = True
-    bpy.ops.object.delete()
-
-def init():
-    render_args = bpy.context.scene.render
-    render_args.engine = "CYCLES"
-    bpy.data.worlds['World'].cycles.sample_as_light = True
-    bpy.context.scene.cycles.blur_glossy = 2.0
-
-def init_all():
-    """init everything we need for rendering
-    an image
-    """
-    scene_setting_init(g_gpu_render_enable)
-    node_setting_init()
-    cam_obj = bpy.data.objects['Camera']
-    cam_obj.rotation_mode = g_rotation_mode
-
-    bpy.data.objects['Lamp'].data.energy = 50
-    bpy.ops.object.lamp_add(type='SUN')
-
-def node_setting_init():
-    """node settings for render rgb images
-    mainly for compositing the background images
-    """
-
-
-    bpy.context.scene.use_nodes = True
-    tree = bpy.context.scene.node_tree
-    links = tree.links
-
-    for node in tree.nodes:
-        tree.nodes.remove(node)
-    
-    image_node = tree.nodes.new('CompositorNodeImage')
-    scale_node = tree.nodes.new('CompositorNodeScale')
-    alpha_over_node = tree.nodes.new('CompositorNodeAlphaOver')
-    render_layer_node = tree.nodes.new('CompositorNodeRLayers')
-    file_output_node = tree.nodes.new('CompositorNodeOutputFile')
-
-    scale_node.space = g_scale_space
-    file_output_node.base_path = g_syn_rgb_folder
-
-    links.new(image_node.outputs[0], scale_node.inputs[0])
-    links.new(scale_node.outputs[0], alpha_over_node.inputs[1])
-    links.new(render_layer_node.outputs[0], alpha_over_node.inputs[2])
-    links.new(alpha_over_node.outputs[0], file_output_node.inputs[0])
-
-def scene_setting_init(use_gpu):
-    """initialize blender setting configurations
-    """
-    
-    sce = bpy.context.scene.name
-    bpy.data.scenes[sce].render.engine = g_engine_type
-    bpy.data.scenes[sce].cycles.film_transparent = g_use_film_transparent
-    #output
-    bpy.data.scenes[sce].render.image_settings.color_mode = g_rgb_color_mode
-    bpy.data.scenes[sce].render.image_settings.color_depth = g_rgb_color_depth
-    bpy.data.scenes[sce].render.image_settings.file_format = g_rgb_file_format
-
-    #dimensions
-    bpy.data.scenes[sce].render.resolution_x = g_resolution_x
-    bpy.data.scenes[sce].render.resolution_y = g_resolution_y
-    bpy.data.scenes[sce].render.resolution_percentage = g_resolution_percentage
-
-    if use_gpu:
-        bpy.data.scenes[sce].render.engine = 'CYCLES' #only cycles engine can use gpu
-        bpy.data.scenes[sce].render.tile_x = g_hilbert_spiral
-        bpy.data.scenes[sce].render.tile_x = g_hilbert_spiral
-        bpy.types.CyclesRenderSettings.device = 'GPU'
-        bpy.data.scenes[sce].cycles.device = 'GPU'
 # obj_filename = '/home/shamitl/blender_stuff/8d458ab12073c371caa2c06fded3ca21/models/model_normalized.obj'
 # target_filename = '/home/shamitl/projects/3dblender/data/shapes/model_normalized.obj'
 
 # root = "/projects/katefgroup/datasets/shamit_shapenet/ShapeNetCore.v2"
-root = "/home/mprabhud/dataset/keypoint_net/models/ShapeNetCore.v2"
 # init()
 # init_all()
 # cars: 02958343
-instance_id_list = []
-
+root = "/home/mprabhud/dataset/keypoint_net/models/ShapeNetCore.v2"
 def job(instances):
-    st()
+    def combine_objects():
+        """combine all objects in the scene
+        """
+        scene = bpy.context.scene
+        obs = []
+
+        for ob in scene.objects:
+        # whatever objects you want to join...
+            if ob.type == 'MESH':
+                obs.append(ob)
+
+        ctx = bpy.context.copy()
+        # one of the objects to join
+        ctx['active_object'] = obs[0]
+        ctx['selected_objects'] = obs
+        # we need the scene bases as well for joining
+        ctx['selected_editable_bases'] = [scene.object_bases[ob.name] for ob in obs]
+        bpy.ops.object.join(ctx)
+
+    def move_obj_above_ground(mesh_obj):
+        minz = 999999.0
+        for vertex in mesh_obj.data.vertices:
+            # object vertices are in object space, translate to world space
+            v_world = mesh_obj.matrix_world * mathutils.Vector((vertex.co[0],vertex.co[1],vertex.co[2]))
+
+            if v_world[2] < minz:
+                minz = v_world[2]
+        
+        mesh_obj.location.z = mesh_obj.location.z - minz + 0.3
+
+    def clear_mesh():
+        """ clear all meshes in the secene
+        """
+        bpy.ops.object.select_all(action='DESELECT')
+        for obj in bpy.data.objects:
+            if obj.type == 'MESH':
+                obj.select = True
+        bpy.ops.object.delete()
+
+    def init():
+        render_args = bpy.context.scene.render
+        render_args.engine = "CYCLES"
+        bpy.data.worlds['World'].cycles.sample_as_light = True
+        bpy.context.scene.cycles.blur_glossy = 2.0
+
+    def init_all():
+        """init everything we need for rendering
+        an image
+        """
+        scene_setting_init(g_gpu_render_enable)
+        node_setting_init()
+        cam_obj = bpy.data.objects['Camera']
+        cam_obj.rotation_mode = g_rotation_mode
+
+        bpy.data.objects['Lamp'].data.energy = 50
+        bpy.ops.object.lamp_add(type='SUN')
+
+    def node_setting_init():
+        """node settings for render rgb images
+        mainly for compositing the background images
+        """
+
+
+        bpy.context.scene.use_nodes = True
+        tree = bpy.context.scene.node_tree
+        links = tree.links
+
+        for node in tree.nodes:
+            tree.nodes.remove(node)
+        
+        image_node = tree.nodes.new('CompositorNodeImage')
+        scale_node = tree.nodes.new('CompositorNodeScale')
+        alpha_over_node = tree.nodes.new('CompositorNodeAlphaOver')
+        render_layer_node = tree.nodes.new('CompositorNodeRLayers')
+        file_output_node = tree.nodes.new('CompositorNodeOutputFile')
+
+        scale_node.space = g_scale_space
+        file_output_node.base_path = g_syn_rgb_folder
+
+        links.new(image_node.outputs[0], scale_node.inputs[0])
+        links.new(scale_node.outputs[0], alpha_over_node.inputs[1])
+        links.new(render_layer_node.outputs[0], alpha_over_node.inputs[2])
+        links.new(alpha_over_node.outputs[0], file_output_node.inputs[0])
+
+    def scene_setting_init(use_gpu):
+        """initialize blender setting configurations
+        """
+        
+        sce = bpy.context.scene.name
+        bpy.data.scenes[sce].render.engine = g_engine_type
+        bpy.data.scenes[sce].cycles.film_transparent = g_use_film_transparent
+        #output
+        bpy.data.scenes[sce].render.image_settings.color_mode = g_rgb_color_mode
+        bpy.data.scenes[sce].render.image_settings.color_depth = g_rgb_color_depth
+        bpy.data.scenes[sce].render.image_settings.file_format = g_rgb_file_format
+
+        #dimensions
+        bpy.data.scenes[sce].render.resolution_x = g_resolution_x
+        bpy.data.scenes[sce].render.resolution_y = g_resolution_y
+        bpy.data.scenes[sce].render.resolution_percentage = g_resolution_percentage
+
+        if use_gpu:
+            bpy.data.scenes[sce].render.engine = 'CYCLES' #only cycles engine can use gpu
+            bpy.data.scenes[sce].render.tile_x = g_hilbert_spiral
+            bpy.data.scenes[sce].render.tile_x = g_hilbert_spiral
+            bpy.types.CyclesRenderSettings.device = 'GPU'
+            bpy.data.scenes[sce].cycles.device = 'GPU'
+    instance_id_list = []
+    root = "/home/mprabhud/dataset/keypoint_net/models/ShapeNetCore.v2"
+    # st()
+    import os
+    # from __future__ import print_function
+    # import ipdb
+    import math, sys, random, argparse, json, os, tempfile, pickle
+    import mathutils
+    import shutil
+    # import pdb
+    import subprocess
+    import os
+    import numpy as np
+    import time
+    import pathos.pools as pp
+    # st = ipdb.set_trace
+    import bpy
+    xmax, ymax, zmax = 5.0, 5.0, 5.0    
+    instances = instances[1]
+    print(instances[0])
     classes = '02958343'
     class_path = os.path.join(root, classes)
     model_dir = os.path.join(class_path, instances, "models")
@@ -175,10 +192,11 @@ def job(instances):
 
 
 classes = '02958343'
+import os
 class_path = os.path.join(root, classes)
 instances = os.listdir(class_path)
 instances = instances[:100]
-p = pp.ProcessPool(1)
+p = pp.ProcessPool(8)
 jobs = sorted(list(enumerate(instances)))
 # st()
 print(jobs)
